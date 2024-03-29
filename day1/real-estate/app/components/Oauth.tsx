ab7a84a901245b1e21dcc24c6fec717485131c35
=tzniceguy <=joeshimbi@gmail.com>
2024-03-29 19:08:46 +0300

    Oauth component

---
+++ day1/real-estate/app/components/Oauth.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Button from './button'
+import Image from 'next/image'
+
+
+export default function Oauth() {
+    const googleButtonText = 'Sign Up with Google';
+    const appleButtonText = 'Sign Up with Apple';
+  return (
+    <div className="mt-5">
+        <div className="border-t-2 m-6">
+        </div>
+        <div>
+        </div> 
+    </div>
+    
+  )
+}
