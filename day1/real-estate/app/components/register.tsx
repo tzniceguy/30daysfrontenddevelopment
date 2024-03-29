4575ebe0ad3983560ce4e8aa64e3ddd0e6863953
=tzniceguy <=joeshimbi@gmail.com>
2024-03-29 19:06:26 +0300

    Registration Form

---
+++ day1/real-estate/app/components/register.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Form from './form'
+export default function Register() {
+  return (
+    <div className="items-center justify-center w-96">
+        <h3 className="text-center text-xl font-semibold">Create your account with Nairobi</h3>
+        <div>
+            <Form />
+        </div>
+    </div>
+  )
+}
