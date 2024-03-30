60284928420da2ea53471fe188edc6e30becd81c
=tzniceguy <=joeshimbi@gmail.com>
2024-03-30 13:38:05 +0300

    importing the svg

--- day1/real-estate/app/components/Oauth.tsx
+++ day1/real-estate/app/components/Oauth.tsx
@@ -1,18 +1,15 @@
 import React from 'react'
 import Button from './button'
 import Image from 'next/image'
-
+import AppleIcon from '../../public/apple-logo.svg'
+import GoogleIcon from '../../public/google-logo.svg'
 
 export default function Oauth() {
-    const googleButtonText = 'Sign Up with Google';
-    const appleButtonText = 'Sign Up with Apple';
   return (
-    <div className="mt-5">
-        <div className="border-t-2 m-6">
-        </div>
-        <div>
-        </div> 
+    <div className="space-y-4">
+      <Button label={`${<Image src={GoogleIcon.src} height={10} width={10} alt='google-icon'  className="text-[#DB4437] mr-2" />} SignUp with Google`} />
+      <Button label={`${<AppleIcon className="text-white mr-2" />} SignUp with Google`} />
     </div>
-    
   )
 }
+ 
