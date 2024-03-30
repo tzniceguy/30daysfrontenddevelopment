d1942af99e684df993a8ad5f4ff2e33c9f90e0df
=tzniceguy <=joeshimbi@gmail.com>
2024-03-30 13:38:40 +0300

    added card to the homepage

--- day1/real-estate/app/page.tsx
+++ day1/real-estate/app/page.tsx
@@ -1,16 +1,15 @@
 import React from "react";
 import Header from "./components/header";
-import Register from "./components/register";
+import Card from "./components/card";
 
 export default function Home() {
   return (
     <main className="md:grid md:grid-cols-12">
-      <div className="container md:col-span-7 h-full flex flex-col justify-center items-center">
+      <div className="container md:col-span-8 h-full flex flex-col justify-center items-center">
           <Header />
-          <Register />
+          <Card />
       </div>
-      <div className="hidden md:flex md:col-span-3 md:h-full md:bg-contain md:bg-image:url('/bg-image.jpg')">
-        background image
+      <div className="hidden md:flex md:col-span-4 md:h-screen md:bg-cover bg-no-repeat" style={{ backgroundImage: `url("/bg-image.jpg")` }}>
       </div>
       
     </main>
