91bbb5a2481c2d243da8102569686a41b8ffb564
=tzniceguy <=joeshimbi@gmail.com>
2024-03-31 13:11:25 +0300

    modified the solumn span for the divisions

--- day1/real-estate/app/page.tsx
+++ day1/real-estate/app/page.tsx
@@ -5,11 +5,11 @@ import Card from "./components/card";
 export default function Home() {
   return (
     <main className="md:grid md:grid-cols-12">
-      <div className="container md:col-span-8 h-full flex flex-col justify-center items-center">
+      <div className="container md:col-span-7 h-full flex flex-col justify-center items-center">
           <Header />
           <Card />
       </div>
-      <div className="hidden md:flex md:col-span-4 md:h-screen md:bg-cover bg-no-repeat" style={{ backgroundImage: `url("/bg-image.jpg")` }}>
+      <div className="hidden md:flex md:col-span-5 md:h-screen md:bg-cover bg-no-repeat" style={{ backgroundImage: `url("/bg-image.jpg")` }}>
       </div>
       
     </main>
