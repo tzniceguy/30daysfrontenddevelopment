3103f590118d8e1878e97374350e6c88e193e687
=tzniceguy <=joeshimbi@gmail.com>
2024-03-31 20:44:21 +0300

    other commit

--- day1/real-estate/app/components/header.tsx
+++ day1/real-estate/app/components/header.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image'
 
 export default function Header() {
   return (
-    <div className="p-6 my-6 float-left">
+    <div className="p-6 my-6 float-left md:text-center">
       <Image src={'./Nairobi-black.svg'} alt="logo" width={100} height={100} className="text-2xl"  />
     </div>
   )
