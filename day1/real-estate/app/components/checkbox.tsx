ab27e98c4fbda1619f570bb16a2f06c1e686a03d
=tzniceguy <=joeshimbi@gmail.com>
2024-04-02 12:37:39 +0300

    other commit

--- day1/real-estate/app/components/checkbox.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function Checkbox() {
-  return (
-    <input type="checkbox" className="p-2 mr-2 "></input>
-  )
-}
--- project2/countdown-page/app/components/input.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function Input() {
-  return (
-    <input type='email' placeholder='Email Address' className="p-4 rounded outline-sky-800 block w-full" />
-  )
-}
