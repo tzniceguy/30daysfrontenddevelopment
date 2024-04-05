3cc2c69909f2a2d1ee1dd40ffca99b7bbfb7592c
=tzniceguy <=joeshimbi@gmail.com>
2024-04-06 00:52:11 +0300

    configured height for devices

--- app/day3/page.tsx
+++ app/day3/page.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Header from './components/header'
 export default function Page() {
   return (
-    <main>
+    <main className="min-h-screen ">
         <Header />
     </main>
   )
