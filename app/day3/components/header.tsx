e14989169014ee1144b19aa88960ffdbbd1d69e6
=tzniceguy <=joeshimbi@gmail.com>
2024-04-06 00:52:55 +0300

    added bytton componnt to the nav

--- app/day3/components/header.tsx
+++ app/day3/components/header.tsx
@@ -2,6 +2,7 @@
 import React, { useState } from 'react'
 import Image from 'next/image'
 import { CiMenuFries } from "react-icons/ci";
+import Button from './button';
 
 
 export default function Header() {
@@ -15,23 +16,34 @@ export default function Header() {
 
     const [isVisible, setVisibility] =useState(false)
 
-  return (
-    <div className="bg-blue-500 p-6 flex flex-wrap space-x-48 text-white">
-        <div>
-            <Image src={"https://sarufi.io/images/logo_w_text.svg"} height={100} width={100} alt='sarufi'/>
-        </div>
-        <nav className='hidden md:flex items-center justify-center space-x-12'>
-            <ul className="flex space-x-6">
-                {navItems.map((navItem) => (
-                    <li key={navItem.href}>{navItem.label}</li>
-                ))}
-            </ul>
+    const toggleMenu =() => {
+        setVisibility!(isVisible)
+    }
 
-            <h2>Login</h2>
-        </nav>
-        <button className="text-2xl">
-            <CiMenuFries />
-        </button>
-    </div>
+  return (
+    <div className="bg-blue-500 p-6 md:flex md:items-center md:justify-between text-white space-x-6">
+        <div className="flex items-center md:space-x-64">
+            <div className="py-5 px-4">
+                <Image src={"https://sarufi.io/images/logo_w_text.svg"} height={200} width={100} alt='sarufi'/>
+            </div>
+            <nav className='hidden md:flex md:items-center md:justify-center'>
+                <ul className=" md:flex md:space-x-6">
+                    {navItems.map((navItem) => (
+                        <li key={navItem.href} className="text-xl hover:bg-gray-400 hover:px-3">{navItem.label}</li>
+                        ))}
+                </ul>
+            </nav>
+        </div>
+        <div className="md:flex md:items-center md:space-x-6">
+            <div className="hidden md:flex md:pb-4 mr-6">
+                <Button label='Login'/>
+            </div>
+        </div>
+        <div className="absolute top-10 right-8 flex md:hidden">
+            <button className="text-3xl" onClick={toggleMenu}>
+                <CiMenuFries />
+            </button>
+        </div>  
+    </div> 
   )
 }
