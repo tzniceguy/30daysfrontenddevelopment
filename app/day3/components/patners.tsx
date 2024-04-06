58a7638911360f74e2ee817ca440b4291fb8f27a
=tzniceguy <=joeshimbi@gmail.com>
2024-04-07 00:12:55 +0300

    patners component

---
+++ app/day3/components/patners.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Patners() {
+  return (
+    <div>
+        <h2 className="text-center text-lg md:text=2xl">Developers from these companies trust us</h2>
+    </div>
+  )
+}
