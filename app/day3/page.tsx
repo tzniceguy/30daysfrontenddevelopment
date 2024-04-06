4365eb4b0b13694f212809ba5ef7413ea5ca6f38
=tzniceguy <=joeshimbi@gmail.com>
2024-04-07 00:13:16 +0300

    added main cmponent to mainpage

--- app/day3/page.tsx
+++ app/day3/page.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import Header from './components/header'
+import Main from './components/main'
 export default function Page() {
   return (
     <main className="min-h-screen ">
         <Header />
+        <Main />
     </main>
   )
 }
