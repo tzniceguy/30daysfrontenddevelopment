31b3ffdeea57cd0822c6a80b5d226623999212e3
=tzniceguy <=joeshimbi@gmail.com>
2024-04-06 23:40:08 +0300

    added humburger menu functionality

--- app/day3/components/header.tsx
+++ app/day3/components/header.tsx
@@ -17,17 +17,17 @@ export default function Header() {
     const [isVisible, setVisibility] =useState(false)
 
     const toggleMenu =() => {
-        setVisibility!(isVisible)
+        setVisibility(!isVisible)
     }
 
   return (
     <div className="bg-blue-500 p-6 md:flex md:items-center md:justify-between text-white space-x-6">
-        <div className="flex items-center md:space-x-64">
+        <div className="md:flex items-center md:space-x-64">
             <div className="py-5 px-4">
                 <Image src={"https://sarufi.io/images/logo_w_text.svg"} height={200} width={100} alt='sarufi'/>
             </div>
-            <nav className='hidden md:flex md:items-center md:justify-center'>
-                <ul className=" md:flex md:space-x-6">
+            <nav className={`md:flex md:items-center md:justify-center ${isVisible ? 'block': 'hidden'} `}>
+                <ul className=" md:flex md:space-x-6 space-y-3">
                     {navItems.map((navItem) => (
                         <li key={navItem.href} className="text-xl hover:bg-gray-400 hover:px-3">{navItem.label}</li>
                         ))}
@@ -35,7 +35,7 @@ export default function Header() {
             </nav>
         </div>
         <div className="md:flex md:items-center md:space-x-6">
-            <div className="hidden md:flex md:pb-4 mr-6">
+            <div className={`md:flex md:pb-4 mr-6 ${isVisible ? 'block': 'hidden'} `}>
                 <Button label='Login'/>
             </div>
         </div>
