efcc50c0d77eaaedfab6c52278dfb1b73f52f1d3
=tzniceguy <=joeshimbi@gmail.com>
2024-04-07 11:08:41 +0300

    init of uber clone

---
+++ app/day4/layout.tsx
@@ -0,0 +1,29 @@
+import type { Metadata } from "next";
+import { Poppins } from "next/font/google";
+import "./globals.css";
+
+const poppins = Poppins({
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-poppins',
+  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900']
+});
+
+export const metadata: Metadata = {
+  title: "Uber Clone",
+  description: "Crafted by Tzniceguy using NextJs",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${poppins.className} min-h-screen w-screen bg-black text-white`}>
+        {children}
+      </body>
+    </html>
+  );
+}
