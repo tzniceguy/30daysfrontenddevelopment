a3ce4909783e0c04f4b852e20fbd090697204856
=tzniceguy <=joeshimbi@gmail.com>
2024-04-07 13:06:16 +0300

    removed class to the layout

--- app/day4/layout.tsx
+++ app/day4/layout.tsx
@@ -21,7 +21,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${poppins.className} min-h-screen w-screen bg-black text-white`}>
+      <body className={poppins.className}>
         {children}
       </body>
     </html>
