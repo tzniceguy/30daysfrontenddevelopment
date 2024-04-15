d6b60c99fdd2cd8082d1c91142491e46ca7d173c
=tzniceguy <=joeshimbi@gmail.com>
2024-04-16 00:36:00 +0300

    added button component

--- app/day5/components/payment.tsx
+++ app/day5/components/payment.tsx
@@ -1,17 +1,18 @@
 import React from 'react'
 import { FaCcVisa } from 'react-icons/fa'
+import Button from './button'
 
 
 export default function Payment() {
   return (
     <div>
-        <h3>Payment Method</h3>
-        <p>Manage billing information and view receipts</p>
+        <h3 className="font-bold text-lg">Payment Method</h3>
+        <p className="">Manage billing information and view receipts</p>
 
         <div className="flex space-x-4">
             <FaCcVisa className="text-5xl"/>
             <span>VISA ending in 2255</span>
-            <span>Remove</span>
+            <Button label='Remove' />
         </div>
     </div>
   )
