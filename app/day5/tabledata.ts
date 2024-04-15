67ff78503e0d65f3921ef3ec40d2027b61cd0749
=tzniceguy <=joeshimbi@gmail.com>
2024-04-15 13:24:58 +0300

    table data initialization

---
+++ app/day5/tabledata.ts
@@ -0,0 +1,6 @@
+export const tableData = [
+    {date: 'Oct. 21. 2021', type: 'Pro Annual'},
+    {date: 'Aug. 21. 2021', type: 'Pro Portifolio'},
+    {date: 'July. 21. 2021', type: 'Sponsored Post'},
+    {date: 'June. 21. 2021', type: 'Sponsored Post'},
+]
