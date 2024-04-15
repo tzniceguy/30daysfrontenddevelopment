141dd0d2b80e5de5290f6a4786634fb2cda00c1a
=tzniceguy <=joeshimbi@gmail.com>
2024-04-16 01:00:40 +0300

    added day5 project details

--- app/components/projects.tsx
+++ app/components/projects.tsx
@@ -7,6 +7,8 @@ export default function Projects() {
         {name: 'Product Launch Page', description: 'A simple Product Launch countdown  inspired by a twitter user on his 30daysDesign challenge', href: 'https://30daysfrontenddevelopment.vercel.app/day2', image: '/project-images/2.png'},
         {name: 'SarufiAI', description: 'Sarufi AI website re-design inspired by a twitter user on his 30daysDesign challenge', href: 'https://30daysfrontenddevelopment.vercel.app/day3', image: '/project-images/3.png'},
         {name: 'Uber Clone', description: 'A uber website clone crafted with NextJS and TailwindCSS', href: 'https://30daysfrontenddevelopment.vercel.app/day4', image: '/project-images/4.png'},
+        {name: 'Billing Page', description: 'A billing page inspired from uidesignsdaily website', href: 'https://30daysfrontenddevelopment.vercel.app/day5', image: '/project-images/5.png'},
+
     ]
   return (
     <div>
