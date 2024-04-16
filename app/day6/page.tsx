e1f7dc62d7d46804cb85506d48fd95fb0dde1b9c
=tzniceguy <=joeshimbi@gmail.com>
2024-04-16 07:23:53 +0300

    project initialization

---
+++ app/day6/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Main from "./components/main";
+
+
+export default function Home() {
+
+  return (
+    <main className="flex min-h-screen flex-col">
+      <Main />
+    </main>
+  );
+}
