cea25bde4aed3e3243a1ced3bd41e1c6773a1c1e
=tzniceguy <=joeshimbi@gmail.com>
2024-04-16 07:40:11 +0300

    remoed an array containing list of projects conducted too a separate file

--- app/components/projects.tsx
+++ app/components/projects.tsx
@@ -1,15 +1,9 @@
 import React from 'react'
 import Card from './card'
+import { projects } from '../projects'
 
 export default function Projects() {
-    const projects =[
-        {name: 'Registration Page', description: 'A simple Registration Page inspired by a twitter user on his 30daysDesign challenge', href: 'https://30daysfrontenddevelopment.vercel.app/day1', image: '/project-images/1.png'},
-        {name: 'Product Launch Page', description: 'A simple Product Launch countdown  inspired by a twitter user on his 30daysDesign challenge', href: 'https://30daysfrontenddevelopment.vercel.app/day2', image: '/project-images/2.png'},
-        {name: 'SarufiAI', description: 'Sarufi AI website re-design inspired by a twitter user on his 30daysDesign challenge', href: 'https://30daysfrontenddevelopment.vercel.app/day3', image: '/project-images/3.png'},
-        {name: 'Uber Clone', description: 'A uber website clone crafted with NextJS and TailwindCSS', href: 'https://30daysfrontenddevelopment.vercel.app/day4', image: '/project-images/4.png'},
-        {name: 'Billing Page', description: 'A billing page inspired from uidesignsdaily website', href: 'https://30daysfrontenddevelopment.vercel.app/day5', image: '/project-images/5.png'},
 
-    ]
   return (
     <div>
         <h4 className="font-bold text-2xl md:text-5xl text-center">Daily Designs</h4>
