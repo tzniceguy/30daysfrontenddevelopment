31c4b5dc585bbe776275f0225539d4a50a90a247
=tzniceguy <=joeshimbi@gmail.com>
2024-04-16 07:24:56 +0300

    navigation list

---
+++ app/day6/navitems.ts
@@ -0,0 +1,7 @@
+export const listItems = [
+    {title: 'Free Designs', href: '/#'},
+    {title: 'Licence', href: '/#'},
+    {title: 'Articles', href: '/#'},
+    {title: 'contributors', href: '/#'},
+    {title: 'about', href: '/#'},
+]
