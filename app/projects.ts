c14c1ffe45808f87fa809626c1182f61102a053e
=tzniceguy <=joeshimbi@gmail.com>
2024-04-16 07:38:59 +0300

    remoed an array containing the list of  projects conducted to a separate file

---
+++ app/projects.ts
@@ -0,0 +1,9 @@
+export const projects = [
+    {name: 'Registration Page', description: 'A simple Registration Page inspired by a twitter user on his 30daysDesign challenge', href: 'https://30daysfrontenddevelopment.vercel.app/day1', image: '/project-images/1.png'},
+    {name: 'Product Launch Page', description: 'A simple Product Launch countdown  inspired by a twitter user on his 30daysDesign challenge', href: 'https://30daysfrontenddevelopment.vercel.app/day2', image: '/project-images/2.png'},
+    {name: 'SarufiAI', description: 'Sarufi AI website re-design inspired by a twitter user on his 30daysDesign challenge', href: 'https://30daysfrontenddevelopment.vercel.app/day3', image: '/project-images/3.png'},
+    {name: 'Uber Clone', description: 'A uber website clone crafted with NextJS and TailwindCSS', href: 'https://30daysfrontenddevelopment.vercel.app/day4', image: '/project-images/4.png'},
+    {name: 'Billing Page', description: 'A billing page inspired from uidesignsdaily website', href: 'https://30daysfrontenddevelopment.vercel.app/day5', image: '/project-images/5.png'},
+    {name: 'UIDesign Daily', description: 'home page for the website uidesigndaily inspired by design aailable at the website', href: 'https://30daysfrontenddevelopment.vercel.app/day6', image: '/project-images/6.png'},
+
+]
